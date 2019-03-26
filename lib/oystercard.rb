4cd3329e7cc259186e609fb3689a0adc55f3cc7a
Phil <phil.board90@googlemail.com>
2019-03-26 15:54:15 +0000

    chapter 11 done

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,31 +1,33 @@
 class Oystercard
+  attr_reader :balance, :entry_station
+
   MAXIMUM_BALANCE = 90
   MIN_BALANCE = 1
   MIN_FARE = 2
-  attr_reader :balance
+
 
   def initialize
     @balance = 0
-    @in_journey = false
+    @entry_station = nil
   end
 
   def top_up(amount)
-    fail 'Maximum balance exceeded' if amount + balance > MAXIMUM_BALANCE
+    fail 'Maximum balance of £#{MAXIMUM_BALANCE} exceeded' if amount + balance > MAXIMUM_BALANCE
     @balance += amount
   end
 
-  def touch_in
+  def touch_in(station)
     fail 'Balance too low' if balance < MIN_BALANCE
-    @in_journey = true
+    @entry_station = station
   end
 
   def touch_out
     deduct(MIN_FARE)
-    @in_journey = false
+    @entry_station = nil
   end
 
   def in_journey?
-    @in_journey
+    !!entry_station #takes its values (true/false) from entry_station
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,65 +2,72 @@
 require 'Oystercard'
 
 describe Oystercard do
-
+  let(:station){ double :station }
   describe '#initialize' do
-    it 'i want money on my card' do
+    it 'it has a balance of zero' do
       expect(subject.balance).to eq 0
-    end
-    it 'raise error if max balance exceeded' do
-      max_balance = Oystercard::MAXIMUM_BALANCE
-      subject.top_up(max_balance)
-      expect{ subject.top_up 1}.to raise_error 'Maximum balance exceeded'
-    end
-    it 'raise error if min balance not met' do
-      min_balance = Oystercard::MIN_BALANCE
-      expect{ subject.touch_in}.to raise_error 'Balance too low'
     end
   end
 
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
-  end
-  it 'i want to top up my card' do
-    subject.top_up(5)
-    expect(subject.balance).to eq 5
+    it 'i want to top up my card' do
+      subject.top_up(5)
+      expect(subject.balance).to eq 5
+    end
+    it 'raise error if max balance exceeded' do
+      max_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(max_balance)
+      expect{ subject.top_up(1) }.to raise_error 'Maximum balance of £#{MAXIMUM_BALANCE} exceeded'
+    end
   end
 
   describe '#touch_in' do
-    describe 'i want to be able to touch in' do
-      it { is_expected.to respond_to(:touch_in) }
-    end
-    it 'card is in use if touch in is called' do
+    it 'in_journey becomes true if touch_in is called' do
       subject.top_up(5)
-      subject.touch_in
+      subject.touch_in(station)
       expect(subject.in_journey?).to be_truthy
     end
-
+    it 'raise error if min balance not met' do
+      min_balance = Oystercard::MIN_BALANCE
+      expect{ subject.touch_in(station) }.to raise_error 'Balance too low'
+    end
+    it 'remeber entry station' do
+      subject.top_up(5)
+      subject.touch_in(station)
+      expect(subject.entry_station).to eq station
+    end
   end
 
   describe '#touch_out' do
-    describe 'i want to be able to touch out' do
-      it { is_expected.to respond_to(:touch_out) }
-    end
-    it 'card is out of use if touch out is called' do
+    it { is_expected.to respond_to(:touch_out) }
+    it 'in_journey becomes false if touch_out is called' do
       subject.top_up(5)
-      subject.touch_in
+      subject.touch_in(station)
       subject.touch_out
       expect(subject.in_journey?).to be_falsey
     end
-    it 'i want to deduct from my card' do
+    it 'deduct fare from card' do
       min_fare = Oystercard::MIN_FARE
       subject.top_up(10)
-      subject.touch_in
+      subject.touch_in(station)
       expect{ subject.touch_out}.to change{ subject.balance }.by -(min_fare)
     end
-
-  describe '#in_journey?' do
-    describe 'i want to be able to touch in' do
-      it { is_expected.to respond_to(:in_journey?) }
+    it 'forgets entry staion on touch out' do
+      subject.top_up(10)
+      subject.touch_in(station)
+      subject.touch_out
+      expect(subject.entry_station).to eq nil
     end
   end
-end
+
+  describe '#in_journey?' do
+    it { is_expected.to respond_to(:in_journey?) }
+  end
+
+
+
+
 
 
 end
